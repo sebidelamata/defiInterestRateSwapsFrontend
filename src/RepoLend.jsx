c5d2da7be60357ec6ce046aa306b035e42181f14
Miguel  de la Mata <sebidelamata@gmail.com>
2024-02-28 10:54:08 -0700

    repolend page css

--- src/RepoLend.jsx
+++ src/RepoLend.jsx
@@ -9,22 +9,34 @@ const RepoLend = () => {
                     RepoLend
                 </div>
                 <ul className="markets-list">
-                    <li className="yt-ausdc-market">
+                    <li className="yt-ausdc-market market-list-item">
                         <div className="market-title">YT aUSDC</div>
                         <ul className="market-stats">
-                            <li className="market-reserve-size">1M YT aUSDC</li>
-                            <li className="market-available-liquidity">400k YT aUSDC</li>
-                            <li className="market-ltv">10%</li>
-                            <li className="market-oracle-price">1 USDC</li>
+                            <li className="market-reserve-size">
+                                <div className="market-reserve-size-title market-stat-title">Market Reserve</div>
+                                <div className="market-reserve-size-amount market-stat-amount">1M YT aUSDC</div>
+                            </li>
+                            <li className="market-available-liquidity">
+                                <div className="market-available-liquidity-title market-stat-title">Available Liquidity</div>
+                                <div className="market-stat-amount market-available-liquidity-amount">400k YT aUSDC</div>
+                            </li>
+                            <li className="market-ltv">
+                                <div className="market-ltv-title market-stat-title">LTV</div>
+                                <div className="market-ltv-amount market-stat-amount">10%</div>
+                            </li>
+                            <li className="market-oracle-price market-stat-title">
+                                <div className="market-oracle-price market-stat-title">Oracle Price:</div>
+                                <div className="market-stat-amount market-oracle-price-amount">1 USDC</div>
+                            </li>
                         </ul>
                         <ul className="user-repo-lend-dashboard">
                             <li className="user-market-wallet-balance">
-                                <div className="user-market-wallet-balance-title">Wallet Balance</div>
-                                <div className="user-market-wallet-balance-amount">50 YT aUSDC</div>
+                                <div className="user-market-wallet-balance-title user-repo-lend-dashboard-item-title">Wallet</div>
+                                <div className="user-market-wallet-balance-amount user-repo-lend-dashboard-item-amount">50 YT aUSDC</div>
                             </li>
                             <div className="market-supply-container">
-                                <div className="market-supply-container">Available to Supply</div>
-                                <div className="market-supply">
+                                <div className="market-supply-container user-repo-lend-dashboard-item-title">Available to Supply</div>
+                                <div className="market-supply user-repo-lend-dashboard-item-amount">
                                     <div className="market-supply-amount">50</div>
                                     <div className="market-supply-unit">YT aUSDC</div>
                                 </div>
@@ -32,7 +44,7 @@ const RepoLend = () => {
                             </div>
                             <div className="market-borrow-container">
                                 <div className="market-borrow-container">Available to Borrow</div>
-                                <div className="market-borrow">
+                                <div className="market-borrow user-repo-lend-dashboard-item-amount">
                                     <div className="market-borrow-amount">50</div>
                                     <div className="market-borrow-unit">YT aUSDC</div>
                                 </div>
